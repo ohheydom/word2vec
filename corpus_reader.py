c94cc0422f945fba3928fec83ee46f40c840bb2c
Dom Leone <ohheydom@gmail.com>
2016-08-17 13:13:26 -0400

    Implement word2vec model         Finish first iteration of working word2vec implementation         Complete methods inside CorpusReader class

--- corpus_reader.py
+++ corpus_reader.py
@@ -1,8 +1,82 @@
+from os import walk
+import string
+
 class CorpusReader:
+    """ CorpusReader takes in a folder name and provides helper methods
+    for the word2vec implementation. 
+    """
+
     def __init__(self, folder, window_size=1):
         self.folder = folder
         self.window_size = window_size
-        pass
+        self.dictionary = {}
+        self.location = 0
+        self.text = []
 
-    def next_batch(batch_size=10):
-        pass
+    def build_dictionary(self):
+        """ build_dictionary Builds the dictionary of all the words from 
+        a given folder. Also builds an array of the entire text corpus.
+
+        Returns
+        -------
+        int : Length of the dictionary
+        """
+
+        for root, directory, files in walk(self.folder):
+            for f in files:
+                with open('{}/{}'.format(root, f)) as of:
+                    for line in of:
+                        for word in line.split():
+                            w = word.lower().strip(string.punctuation)
+                            self.text.append(w)
+                            if w not in self.dictionary:
+                                self.dictionary[w] = len(self.dictionary)
+        return len(self.dictionary)
+
+
+    def next_batch(self, batch_size=10):
+        """ next_batch Returns a 2 dimensional list of length batch_size like so:
+        [[word, context], [word, context], [word, context]]
+        The words and contexts are the indices of the words from
+        the dictionary mapping.
+
+        Returns
+        -------
+        list : shape(batch_size, 2)
+        """
+
+        l = []
+        text_len = len(self.text)
+        for _ in self.text:
+            window = 1
+            idx = self.location
+            self.location += 1
+            if idx >= text_len:
+                self.location = 0
+                continue
+            if idx == 0:
+                while window <= self.window_size and idx+window <= text_len-1:
+                    l.append([self.dictionary[self.text[idx]], self.dictionary[self.text[idx+window]]])
+                    if len(l) == batch_size:
+                        return l
+                    window += 1
+                continue
+            if idx == text_len-1:
+                while window <= self.window_size and idx-window >= 0:
+                    l.append([self.dictionary[self.text[idx]], self.dictionary[self.text[idx-1-window]]])
+                    if len(l) == batch_size:
+                        return l
+                    window += 1
+                continue
+            while window <= self.window_size and idx-window >= 0:
+                l.append([self.dictionary[self.text[idx]], self.dictionary[self.text[idx-window]]])
+                if len(l) == batch_size:
+                    return l
+                window += 1
+            window = 1
+            while window <= self.window_size and idx+window <= text_len-1:
+                l.append([self.dictionary[self.text[idx]], self.dictionary[self.text[idx+window]]])
+                if len(l) == batch_size:
+                    return l
+                window += 1
+        return l
--- word2vec.py
+++ word2vec.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import numpy as np
 from corpus_reader import CorpusReader
 
-batch_size = 20
-neg_samples = 10
-embedding_size = 50
-vocabulary_size = 100
+batch_size = 100
+neg_samples = 40
+embedding_size = 200
+window_size = 1
 
 def init_weights(shape):
     init = tf.truncated_normal(shape, stddev = 0.1)
@@ -15,8 +15,12 @@ def init_biases(shape):
     init = tf.constant(0.1, shape=shape)
     return tf.Variable(init)
 
+corpus = CorpusReader('data', window_size=window_size)
+vocabulary_size = corpus.build_dictionary()
+
 X_train = tf.placeholder(tf.int32, [batch_size])
-y_train = tf.placeholder(tf.int32, [batch_size, 1])
+y_train = tf.placeholder(tf.int32, [batch_size])
+y = tf.reshape(y_train, [-1, 1])
 
 embeddings = init_weights([vocabulary_size, embedding_size])
 W = init_weights([vocabulary_size, embedding_size])
@@ -24,11 +28,12 @@ b = init_biases([vocabulary_size])
 
 batch_embed = tf.nn.embedding_lookup(embeddings, X_train)
 
-loss = tf.nn.nce_loss(W, b, batch_embed, y_train, neg_samples, vocabulary_size)
+loss = tf.reduce_mean(tf.nn.sampled_softmax_loss(W, b, batch_embed, y, neg_samples, vocabulary_size))
 train = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
-#with tf.Session() as sess:
-#    sess.run(tf.initialize_all_variables())
-#    for _ in range(20):
-#        batch = None
-#        sess.run(train, feed_dict={X_train: batch[0], y_train: batch[1]})
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+    for _ in range(2000):
+        word, context = zip(*corpus.next_batch(batch_size))
+        l, _ = sess.run([loss, train], feed_dict={X_train: word, y_train: context})
+        print l
